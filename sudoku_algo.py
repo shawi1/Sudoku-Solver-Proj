14738b362f977596da52855f243530370962d10b
Sean Hawi <seanhawi2004@gmail.com>
2025-02-24 14:17:06 -0800

    Valid check done

--- sudoku_algo.py
+++ sudoku_algo.py
@@ -1,7 +1,7 @@
 # Python algorithm to solve a Sudoku Board
 
 # First check if it is valid to place num at board[row][col]
-def isValid(board, row, col, num):
+def is_valid(board, row, col, num):
     # check if num is in row
     for x in range(9):
         if board[row][x] == num:
@@ -13,5 +13,26 @@ def isValid(board, row, col, num):
             return False
         
     # check if num is in the 3x3 sub-matrix
-    startRow = row - (row % 3)
-    startCol = col - (col % 3)
+    start_row = row - (row % 3)
+    start_col = col - (col % 3)
+
+    for i in range(3):
+        for j in range(3):
+            if board[i + start_row][j + start_col] == num:
+                return False
+            
+
+    return True
+
+
+def sudoku_algorithm(board, row, col):
+    # base case:
+    if row == 8 and col == 9:
+        return True
+    
+    # check if last column of the row, go to next row
+    if col == 9:
+        row += 1
+        col = 0
+
+        
